11e44ba7de6d7efc71d0d4700262f202268c67e9
mihaildicoski <133096633+mihaildicoski@users.noreply.github.com>
2024-05-29 15:26:15 +0200

    Active headings go bold

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -1,19 +1,42 @@
 <script>
     import "../app.css";
     import { goto } from '$app/navigation';
+    import { page } from '$app/stores';
+    import { derived } from "svelte/store";
+
+    let activebutton = '';
+
+    const currentpath = derived(page, $page => $page.url.pathname);
+
+    $: activebutton = $currentpath; 
+
+    // $: console.log(activebutton);
+
+    
+
+    
+
+
   </script>
   
   <style>
     nav a {
       margin-right: 1rem;
     }
+    nav a:focus{
+      font-weight: bold; 
+    }
+    .active{
+      font-weight: bold; 
+    }
+
   </style>
   
   <header class="bg-gray-800 text-white p-4">
     <nav class="container mx-auto">
-      <a href="/Dashboard" class="text">Dashboard</a>
-      <a href="/GridSimulation">Grid Simulation</a>
-      <a href="/BiddingMarket">Bidding Market</a>
+      <a href="/Dashboard" class = {activebutton == '/Dashboard' ? 'active' : ''}>Dashboard</a>
+      <a href="/GridSimulation" class = {activebutton == '/GridSimulation' ? 'active' : ''}>Grid Simulation</a>
+      <a href="/BiddingMarket" class = {activebutton == '/BiddingMarket' ? 'active' : ''}>Bidding Market</a>
     </nav>
   </header>
   

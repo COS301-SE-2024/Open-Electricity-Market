2849be19381f214910193f5800f3cc435ae63f0b
Simon Swart <u22490729@tuks.co.za>
2024-05-31 10:16:51 +0200

    Fix some lint errors

--- backend/market-platform/src/main.rs
+++ backend/market-platform/src/main.rs
@@ -7,10 +7,9 @@ use rocket::State;
 use std::sync::atomic::{AtomicU32, Ordering};
 use std::sync::{Arc, Mutex};
 
-
+use rocket::fairing::{Fairing, Info, Kind};
 use rocket::http::Header;
 use rocket::{Request, Response};
-use rocket::fairing::{Fairing, Info, Kind};
 
 pub struct CORS;
 
@@ -19,13 +18,16 @@ impl Fairing for CORS {
     fn info(&self) -> Info {
         Info {
             name: "Add CORS headers to responses",
-            kind: Kind::Response
+            kind: Kind::Response,
         }
     }
 
     async fn on_response<'r>(&self, _request: &'r Request<'_>, response: &mut Response<'r>) {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new(
+            "Access-Control-Allow-Methods",
+            "POST, GET, PATCH, OPTIONS",
+        ));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
     }
@@ -109,5 +111,4 @@ fn rocket() -> _ {
         })
         .manage(Arc::new(Mutex::new(Vec::<String>::new())))
         .attach(CORS)
-
 }

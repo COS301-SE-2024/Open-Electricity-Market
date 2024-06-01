77842efaa06689ab895dc7f46c6c9e4742a9ae66
mihaildicoski <133096633+mihaildicoski@users.noreply.github.com>
2024-06-01 21:11:45 +0200

    Added functionality for bids to be added and then removed when met.

--- Frontend/src/routes/Main/BiddingMarket/+page.svelte
+++ Frontend/src/routes/Main/BiddingMarket/+page.svelte
@@ -1,6 +1,15 @@
 <script>
   import { onMount } from "svelte";
+  import { uid } from "uid";
   let interval;
+
+  // console.log(uid());
+  var arrayofuids = [];
+  var arrayofpricesbought = [];
+  // const userid = uid();
+
+
+  
 
   onMount(async () => {
     await checkMet();
@@ -12,25 +21,44 @@
     };
   });
 
-  async function checkMet(id) {
-    const response = await fetch("http://localhost:8001/met/temp");
-    let text = await response.text();
-    if (text == "true") {
-      alert("Bid met");
+  async function removeelement(element){
+    for(let i = 0; i<arrayofuids.length; i++){
+      let tempy = arrayofuids[i]; 
+      if (tempy == element) {
+        arrayofuids.splice(i, 1);
+      }
+    }
+  }
+
+  async function checkMet() {
+    for (let i = 0; i < arrayofuids.length; i++) {
+      const element = arrayofuids[i];
+      const response = await fetch(`http://localhost:8001/met/${element}`);
+      let text = await response.text();
+      // console.log(text);
+      if (text == "true") {
+        console.log(`Bid met for agent with id of: ${element}`);
+        removeelement(element);
+      }
     }
   }
 
   async function bidFunction() {
     //https://api.coindesk.com/v1/bpi/currentprice.json
-    const response = await fetch("http://localhost:8001/bid/1118/100/temp"); //have to insert endpoint
+    let id = uid();
+    arrayofuids.push(id);
+    console.log(id);
+    console.log(arrayofuids);
+    const response = await fetch(`http://localhost:8001/bid/1118/100/${id}`); //have to insert endpoint
     let text = await response.text();
     console.log(text);
+
     // data = JSON.parse(text)
   }
 
   async function sellFunction() {
     //https://api.coindesk.com/v1/bpi/currentprice.json
-    const response = await fetch("http://localhost:8001/sell/2000"); //have to insert endpoint
+    const response = await fetch("http://localhost:8001/sell/1500"); //have to insert endpoint
     let text = await response.text();
     console.log(text);
     // data = JSON.parse(text)
@@ -40,8 +68,30 @@
 <main class="container mx-auto">
   <h1 class="text-2xl font-bold mb-4">Bidding Market</h1>
 
-  <button on:click={bidFunction} class="btn bg-green-600">Bid</button>
-  <button on:click={sellFunction} class="btn bg bg-red-700">Sell</button>
+  <div class="overflow-x-auto">
+    <table class="table table-xs">
+      <thead>
+        <tr>
+          <th></th>
+          <th>Id</th>
+          <th>Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th>1</th>
+          <td>363b5e030e2</td>
+          <td>R230.45</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <button on:click={bidFunction} class="btn bg-green-600 mt-20 mr-1 w-1/6"
+      >Bid</button
+    >
+    <button on:click={sellFunction} class="btn bg bg-red-700 w-1/6">Sell</button
+    >
+  </div>
 </main>
 
 <style>

d5198be05c1d6809b835f4ce55a89063a40cd63d
mihaildicoski <133096633+mihaildicoski@users.noreply.github.com>
2024-06-02 16:55:10 +0200

    ..cleaned layout.svelte

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -18,25 +18,11 @@
 </script>
 
 <style>
-  nav a {
-    margin-right: 1rem;
-  }
-  nav a:focus{
-    font-weight: bold; 
-  }
-  .active{
-    font-weight: bold; 
-  }
+ 
 
 </style>
 
-<!-- <header class="bg-gray-800 text-white p-4">
-  <nav class="container mx-auto">
-    <a href="/Dashboard" class = {activebutton == '/Dashboard' ? 'active' : ''}>Dashboard</a>
-    <a href="/GridSimulation" class = {activebutton == '/GridSimulation' ? 'active' : ''}>Grid Simulation</a>
-    <a href="/BiddingMarket" class = {activebutton == '/BiddingMarket' ? 'active' : ''}>Bidding Market</a>
-  </nav>
-</header> -->
+
 
 <main class="container mx-auto ">
   <slot />

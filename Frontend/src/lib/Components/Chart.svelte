b68f9c08c04f7cfdba39a15ee391eb5b9c4f0f10
Daniel Pierre de Villiers <u22509268@tuks.co.za>
2024-06-11 16:10:45 +0200

    Merge branch 'feature/gridsimulationfront' of https://github.com/COS301-SE-2024/Open-Electricity-Market into feature/gridsimulationfront

--- Frontend/src/lib/Components/Chart.svelte
+++ Frontend/src/lib/Components/Chart.svelte
@@ -1,33 +1,32 @@
 
 <script>
-    import { onMount } from 'svelte';
-    import { createChart, sampleChartConfig } from './Chart.js';
-  
-    let chart;
-    let chartCanvas;
-  
-    onMount(() => {
-      if (typeof window !== 'undefined') { // Check if running in the browser
-        chart = createChart(chartCanvas, sampleChartConfig);
-      }
-      
-      return () => {
-        if (chart) {
-          chart.destroy();
-      
-        }
-      };
-    });
-  </script>
-  
-  <div style="display: flex; max-width: 50%;"> 
-    <canvas bind:this={chartCanvas} height="300"></canvas>
-  </div>
-  
-  <style>
-    canvas {
-      max-width: 100%;
-      max-height: 100%;
+  import { onMount } from 'svelte';
+  import { createChart, sampleChartConfig } from './Chart.js';
+
+  let chart;
+  let chartCanvas;
+
+  onMount(() => {
+    if (typeof window !== 'undefined') { // Check if running in the browser
+      chart = createChart(chartCanvas, sampleChartConfig);
     }
-  </style>
+
+    return () => {
+      if (chart) {
+        chart.destroy();
+      }
+    };
+  });
+</script>
+
+<div style="display: flex; max-width: 50%;"> 
+  <canvas bind:this={chartCanvas} height="300"></canvas>
+</div>
+
+<style>
+  /* canvas {
+    max-width: 100%;
+    max-height: 100%;
+  } */
+</style>
   
--- Frontend/src/routes/Main/+layout.svelte
+++ Frontend/src/routes/Main/+layout.svelte
@@ -15,29 +15,29 @@
     
 
 
-  </script>
+</script>
   
-  <style>
-    nav a {
-      margin-right: 1rem;
-    }
-    nav a:focus{
-      font-weight: bold; 
-    }
-    .active{
-      font-weight: bold; 
-    }
+<style>
+  nav a {
+    margin-right: 1rem;
+  }
+  nav a:focus{
+    font-weight: bold; 
+  }
+  .active{
+    font-weight: bold; 
+  }
 
-  </style>
-  
-  <header class="bg-gray-800 text-white p-4 ">
-    <nav class="container mx-auto">
-      <a href="/Main/Dashboard" class = {activebutton == '/Main/Dashboard' ? 'active' : ''}>Dashboard</a>
-      <a href="/Main/GridSimulation" class = {activebutton == '/Main/GridSimulation' ? 'active' : ''}>Grid Simulation</a>
-      <a href="/Main/BiddingMarket" class = {activebutton == '/Main/BiddingMarket' ? 'active' : ''}>Bidding Market</a>
-    </nav>
-  </header>
-  
-  <main class="container mx-auto mt-8">
-    <slot />
-  </main>
+</style>
+
+<header class="bg-gray-800 text-white p-4 ">
+  <nav class="container mx-auto">
+    <a href="/Main/Dashboard" class = {activebutton == '/Main/Dashboard' ? 'active' : ''}>Dashboard</a>
+    <a href="/Main/GridSimulation" class = {activebutton == '/Main/GridSimulation' ? 'active' : ''}>Grid Simulation</a>
+    <a href="/Main/BiddingMarket" class = {activebutton == '/Main/BiddingMarket' ? 'active' : ''}>Bidding Market</a>
+  </nav>
+</header>
+
+<main class="container mx-auto mt-8">
+  <slot />
+</main>

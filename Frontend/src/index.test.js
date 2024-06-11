7f64509000f91ea6757edc1632aaa8352ef9c2aa
Daniel Pierre de Villiers <u22509268@tuks.co.za>
2024-06-11 14:48:09 +0200

    Prettier for vs is running and test setup

--- Frontend/src/index.test.js
+++ Frontend/src/index.test.js
@@ -1,21 +1,28 @@
-import { describe, it, expect } from 'vitest';
-import BiddingMarket from './routes/Main/BiddingMarket/+page.svelte';
-import {removeelement2} from './routes/Main/BiddingMarket/+page.svelte';
+import { describe, it, expect } from "vitest";
+import { render } from "@testing-library/svelte";
+import BiddingMarket from "./routes/Main/BiddingMarket/+page.svelte";
 
-
-describe('sum test', () => {
-	it('adds 1 + 2 to equal 3', () => {
-		expect(1 + 2).toBe(3);
-	});
+describe("sum test", () => {
+  it("adds 1 + 2 to equal 3", () => {
+    expect(1 + 2).toBe(3);
+  });
 });
 
-describe('Remove element test', () => {
-	it('removes an element from an array of uids', () => {
-	  const arrayofuids = [1, 2, 3, 4, 5];
-	  const elementToRemove = 3;
-	  const expectedArray = [1, 2, 4, 5];
-	  const result = removeelement2(arrayofuids, elementToRemove);
-	  expect(result).toEqual(expectedArray);
-	});
-});
+// describe("Remove element test", () => {
+//   it("removes an element from an array of uids", () => {
+//     const arrayofuids = [1, 2, 3, 4, 5];
+//     const elementToRemove = 3;
+//     const expectedArray = [1, 2, 4, 5];
 
+//     const component = render(BiddingMarket);
+
+//     expect(component.removeelement2(arrayofuids, elementToRemove)).toEqual(
+//       expectedArray,
+//     );
+//   });
+// });
+
+// test("it works", async () => {
+//   const component = render(MyComponent);
+//   expect(component.veryComplicated("foo")).toBe("bar");
+// });

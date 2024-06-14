f77da66fe8504a5eaec11d78be1991cb7b565f73
anyx66 <u22498878@tuks.co.za>
2024-06-14 19:03:01 +0200

    Background sides version. Still sorting out contents

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -13,8 +13,8 @@
     let password = '';
 </script>
 
-<div class = "h-screen w-screen bg-cover bg-no-repeat" style="margin-left:-172px; background-image: url('../src/lib/assets/pylonBack.png');">
-    <div class = "bg-black/95 bg-cover">
+<div class = "h-screen bg-cover bg-no-repeat" style="background-image: url('../src/lib/assets/pylonBack.png');">
+    <div class = "bg-black/95 bg-cover bg-center h-screen">
         <img class= "h-auto max-w-full" alt = "Logo" src = {logo}/>
         <h1 class ="mb-4 text-heading1 font-heading1">Welcome To OpenElectricityMarket!</h1>
         <h2 class = "mb-4 text-heading2 font-heading2">Log In</h2>

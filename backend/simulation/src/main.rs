1a2197aa4308ff605d3827ad864e7e0e83e388e3
Simon Swart <u22490729@tuks.co.za>
2024-06-14 21:13:49 +0200

    Underlying simulation now working
    Everything now redone in a more rust style.

--- backend/simulation/src/main.rs
+++ backend/simulation/src/main.rs
@@ -9,175 +9,121 @@ use std::sync::atomic::AtomicU64;
 use std::thread::current;
 use rocket::serde::json::json;
 use rocket::yansi::Paint;
+use std::time::{Duration, Instant};
 
 
-struct Location<'a> {
-  equipment: Vec<GridEquipment<'a>>,
-
+struct Generator {
+    id : u32,
+    voltage: Voltage,
+    max_voltage:f32,
+    frequency :f32,
+    transmission_line: u32,
 }
 
-trait ACEquipment {
-    fn update(&mut self, elapsed_time: f32);
-    fn get_json(&self) -> String;
+struct Consumer {
+    id : u32,
+    resistance: Resistance,
+    transmission_line: u32,
+    voltage: Voltage
 }
 
-enum GridEquipment<'a> {
-    Consumer(Consumer<'a>),
-    ACSwitches(ACSwitches),
-    Transformers(Transformers<'a>),
-    Generators(Generators<'a>)
+struct TransmissionLine {
+    id : u32,
+    resistance: Resistance,
+    impedance: Resistance,
+    voltage: Voltage
 }
 
-struct Consumer<'a> {
-    supplier:  &'a Arc<Mutex<TransmissionLines>>,
-    load : f32
-}
-
-impl ACEquipment for Consumer<'_> {
-    fn update(&mut self, elapsed_time: f32) {
-        let mut supplier_voltage =self.supplier.lock().unwrap();
-
-        if supplier_voltage.voltage.phase1 > supplier_voltage.voltage.phase2 && supplier_voltage.voltage.phase1 > supplier_voltage.voltage.phase3{
-            self.load = supplier_voltage.voltage.phase1;
-        } else if supplier_voltage.voltage.phase2 > supplier_voltage.voltage.phase3 {
-            self.load = supplier_voltage.voltage.phase2;
-        } else {
-            self.load = supplier_voltage.voltage.phase3;
-        }
-    }
-
-    fn get_json(&self) -> String {
-        todo!()
-    }
-}
-
-
-struct ACSwitches {
-    line_in: Box<TransmissionLines>,
-    line_out:  Box<TransmissionLines>,
-    open : bool
-}
-
-impl ACEquipment  for ACSwitches  {
-    fn update(&mut self, elapsed_time: f32) {
-        if (self.open){
-            self.line_out.voltage.phase1 = self.line_in.voltage.phase1;
-            self.line_out.voltage.phase2 = self.line_in.voltage.phase2;
-            self.line_out.voltage.phase3 = self.line_in.voltage.phase3;
-        } else {
-            self.line_in.voltage.phase1 = 0.0;
-            self.line_in.voltage.phase2 = 0.0;
-            self.line_in.voltage.phase3 = 0.0;
-
-            self.line_out.voltage.phase1 = 0.0;
-            self.line_out.voltage.phase2 = 0.0;
-            self.line_out.voltage.phase3 = 0.0;
-        }
-        self.line_out.update(elapsed_time);
-        self.line_in.update(elapsed_time);
-    }
-
-    fn get_json(&self) -> String {
-        todo!()
-    }
-}
-
-struct TransmissionLines {
-    current: Current3Phase,
-    resistance: f32,
-    voltage : Voltage3Phase
-}
-
-impl ACEquipment  for TransmissionLines {
-    fn update(&mut self, _elapsed_time: f32) {
-        self.current.phase1 = self.voltage.phase1/self.resistance;
-        self.current.phase2 = self.voltage.phase2/self.resistance;
-        self.current.phase3 = self.voltage.phase3/self.resistance;
-    }
-
-    fn get_json(&self) -> String {
-        json!({"Type":"Transmission Line",
-               "Voltages":{
-                "Phase1":self.voltage.phase1,
-                "Phase2":self.voltage.phase2,
-                "Phase3":self.voltage.phase3
-                },
-               "Current":{
-                "Phase1":self.current.phase1,
-                "Phase2":self.current.phase2,
-                "Phase3":self.current.phase3
-                },
-        }).to_string()
-    }
-}
-
-
-struct Transformers<'a> {
+struct Transformer {
+    id : u32,
     ratio : f32,
-    primary: &'a Arc<Mutex<TransmissionLines>>,
-    secondary: &'a Arc<Mutex<TransmissionLines>>
+    primary : u32,
+    secondary : u32,
 }
 
-impl ACEquipment  for Transformers<'_>  {
-    fn update(&mut self, elapsed_time: f32) {
-        let mut secondary = self.secondary.lock().unwrap();
-        let mut primary = self.primary.lock().unwrap();
-        secondary.voltage.phase1 = primary.voltage.phase1*self.ratio;
-        secondary.voltage.phase2 = primary.voltage.phase2*self.ratio;
-        secondary.voltage.phase3 = primary.voltage.phase3*self.ratio;
-        secondary.update(elapsed_time);
+struct Resistance(f32);
+struct Voltage(f32,f32,f32);
+
+
+struct Grid {
+    consumers : Vec<Consumer>,
+    transmission_lines: Vec<TransmissionLine>,
+    generators : Vec<Generator>,
+    transformers: Vec<Transformer>,
+    started : bool
+}
+
+impl Grid {
+    fn update_impedance(&mut self) {
+        for line in self.transmission_lines.iter_mut() {
+            let index =self.consumers.iter().position( |c| c.transmission_line == line.id);
+            match index {
+                None => {line.impedance = Resistance(line.resistance.0)}
+                Some(i) => {line.impedance = Resistance(line.resistance.0+self.consumers[i].resistance.0)}
+            }
+
+        }
     }
 
-    fn get_json(&self) -> String {
-        json!({"Ratio":self.ratio}).to_string()
-    }
-}
-
-struct Generators<'a> {
-    voltage : Voltage3Phase,
-    frequency : f32,
-    line_out:  &'a Arc<Mutex<TransmissionLines>>,
-    max_voltage : f32
-}
-
-impl ACEquipment  for Generators<'_>  {
-    fn update(&mut self, elapsed_time: f32) {
-        self.voltage.phase1 = self.max_voltage*f32::sin(self.frequency*std::f32::consts::FRAC_2_PI*elapsed_time);
-        self.voltage.phase2 = self.max_voltage*f32::sin(self.frequency*std::f32::consts::FRAC_2_PI*elapsed_time-(std::f32::consts::FRAC_2_PI/3.0));
-        self.voltage.phase3 = self.max_voltage*f32::sin(self.frequency*std::f32::consts::FRAC_2_PI*elapsed_time-(2.0*std::f32::consts::FRAC_2_PI/3.0));
-
-        let mut line_out = self.line_out.lock().unwrap();
-
-        line_out.voltage.phase1 =  self.voltage.phase1;
-        line_out.voltage.phase2 =  self.voltage.phase2;
-        line_out.voltage.phase3 =  self.voltage.phase3;
-
-        line_out.update(elapsed_time);
+    fn update_generator_voltages(&mut self, elapsed_time:f32){
+        for gen in self.generators.iter_mut() {
+            gen.voltage.0 = gen.max_voltage*f32::sin(gen.frequency*std::f32::consts::FRAC_2_PI*elapsed_time);
+            gen.voltage.1 = gen.max_voltage*f32::sin(gen.frequency*std::f32::consts::FRAC_2_PI*elapsed_time-(std::f32::consts::FRAC_2_PI/3.0));
+            gen.voltage.2 = gen.max_voltage*f32::sin(gen.frequency*std::f32::consts::FRAC_2_PI*elapsed_time-(2.0*std::f32::consts::FRAC_2_PI/3.0));
+        }
     }
 
-    fn get_json(&self) -> String {
-        json!({ "Voltages":{
-                "Phase1":self.voltage.phase1,
-                "Phase2":self.voltage.phase2,
-                "Phase3":self.voltage.phase3
-                },
-            "Frequency" : self.frequency,
-            "Max voltages" : self.max_voltage
-        }).to_string()
+    fn sync_voltages(&mut self) {
+        // Gens to lines
+        for gen in self.generators.iter_mut() {
+            let index = self.transmission_lines.iter().position( |l| l.id == gen.transmission_line);
+            match index {
+                None => {}
+                Some(i) => {
+                    self.transmission_lines[i].voltage.0 = gen.voltage.0;
+                    self.transmission_lines[i].voltage.1 = gen.voltage.1;
+                    self.transmission_lines[i].voltage.2 = gen.voltage.2;
+                }
+            }
+        }
+        // Steps
+        for trans in self.transformers.iter() {
+            let primary_index = self.transmission_lines.iter().position(|l| l.id == trans.primary);
+            let secondary_index = self.transmission_lines.iter().position(|l| l.id == trans.secondary);
+            match primary_index {
+                None => {}
+                Some(i) => {
+                    match secondary_index {
+                        None => {}
+                        Some(j) => {
+                            self.transmission_lines[j].voltage.0 = self.transmission_lines[i].voltage.0*trans.ratio;
+                            self.transmission_lines[j].voltage.1 = self.transmission_lines[i].voltage.1*trans.ratio;
+                            self.transmission_lines[j].voltage.2 = self.transmission_lines[i].voltage.2*trans.ratio;
+                        }
+                    }
+
+                }
+            }
+        }
+
+        // Lines to consumers
+        for line in self.transmission_lines.iter_mut() {
+            let index = self.consumers.iter().position( |c| c.transmission_line == line.id);
+            match index {
+                None => {}
+                Some(i) => {
+                    self.consumers[i].voltage.0 = line.voltage.0;
+                    self.consumers[i].voltage.1 = line.voltage.1;
+                    self.consumers[i].voltage.2 = line.voltage.2;
+                }
+            }
+        }
+
+
     }
-}
-
-struct Voltage3Phase {
-    phase1 : f32,
-    phase2 : f32,
-    phase3 : f32
 
 }
-struct Current3Phase {
-    phase1 : f32,
-    phase2 : f32,
-    phase3 : f32
-}
+
 
 
 #[get("/")]
@@ -196,80 +142,50 @@ fn consume(amount: u64) -> String {
 }
 
 #[get("/start")]
-fn start(grid: &State<Arc<Mutex<Location>>>) -> String {
-    let mut elapsed_time = 0.1;
-    loop {
-
-        let mut grid = grid.lock().unwrap();
-
-        for g in grid.equipment.iter_mut() {
-            match g {
-                GridEquipment::Consumer(c) => {c.update(elapsed_time)}
-                GridEquipment::ACSwitches(_) => {}
-                GridEquipment::Transformers(t) => {t.update(elapsed_time)}
-                GridEquipment::Generators(g) => {g.update(elapsed_time)}
+fn start(grid: &State<Arc<Mutex<Grid>>>) -> String {
+    let mut g = grid.lock().unwrap();
+    if !g.started {
+        g.started = true;
+        let clone = grid.inner().clone();
+        tokio::spawn(async move{
+            let mut start = Instant::now();
+            let mut elapsed_time = 0.0;
+            loop {
+                let duration = start.elapsed();
+                elapsed_time += duration.as_secs_f32();
+                let mut grid = clone.lock().unwrap();
+                grid.update_impedance();
+                grid.update_generator_voltages(elapsed_time);
+                grid.sync_voltages();
+                start = Instant::now();
             }
-        }
-        elapsed_time += 0.1;
+        });
+        "Grid Started".to_string()
+    } else {
+        "Grid Already Running".to_string()
     }
 }
 
 #[launch]
 fn rocket() -> _ {
-    println!("Hi");
-    let line1 = Arc::new(Mutex::new(TransmissionLines {
-        current: Current3Phase {
-            phase1: 0.0,
-            phase2: 0.0,
-            phase3: 0.0,
-        },
-        resistance: 5.0,
-        voltage: Voltage3Phase {
-            phase1: 0.0,
-            phase2: 0.0,
-            phase3: 0.0,
-        },
-    })) ;
-
-    let line2 = Arc::new(Mutex::new(TransmissionLines {
-        current: Current3Phase {
-            phase1: 0.0,
-            phase2: 0.0,
-            phase3: 0.0,
-        },
-        resistance: 5.0,
-        voltage: Voltage3Phase {
-            phase1: 0.0,
-            phase2: 0.0,
-            phase3: 0.0,
-        },
-    }));
-
-
-    let mut gen1 = Generators {
-        voltage: Voltage3Phase {
-            phase1: 0.0,
-            phase2: 0.0,
-            phase3: 0.0,
-        },
-        frequency: 50.0,
-        line_out: & line1,
-        max_voltage: 230.0,
-    };
-
-
-    let mut trans1 = Transformers{
-        ratio: 1.0,
-        primary: &line1,
-        secondary: &line2,
-    };
-
-    let mut house = Consumer { supplier:&line2, load: 0.0 };
-
-    let mut power_test = Location { equipment: vec![GridEquipment::Generators(gen1),GridEquipment::Transformers(trans1),GridEquipment::Consumer(house)] };
-
-
     rocket::build()
-        .mount("/", routes![index, produce, consume])
-        .manage(Arc::new(Mutex::new(power_test)))
+        .mount("/", routes![index, produce, consume,start])
+        .manage(Arc::new(Mutex::new(Grid {
+            consumers: vec![
+                Consumer {id: 1,resistance: Resistance(1000.0),transmission_line: 1, voltage: Voltage(0.0,0.0,0.0)}
+            ],
+            transmission_lines: vec![
+                TransmissionLine {id:0,resistance: Resistance(50.0) , impedance: Resistance(0.0), voltage: Voltage(0.0, 0.0, 0.0)},
+                TransmissionLine {id:1,resistance: Resistance(50.0) , impedance: Resistance(0.0), voltage: Voltage(0.0, 0.0, 0.0)}
+
+            ],
+            generators: vec![
+                Generator {id:0,voltage: Voltage(0.0,0.0,0.0), max_voltage: 240.0, frequency: 50.0, transmission_line: 0 }
+
+            ],
+            transformers: vec![
+                Transformer {id:0,ratio:0.5,primary:0,secondary:1}
+            ],
+            started: false,
+        })))
 }

dde30d37c6dffe81c7a115d3f6a3eafe87941f67
Daniel Pierre de Villiers <u22509268@tuks.co.za>
2024-06-19 18:52:35 +0200

    tailwind config adjustments
    We should be able to apply different themes from a selection of default ones

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -15,8 +15,8 @@
 
 <main>
     <div class="hero min-h-screen" style="background-image: url(https://images.unsplash.com/photo-1510595256055-e44b27efe497?q=80&w=1700&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D);">
-        <div class="hero-overlay bg-opacity-60"></div>
-        <div class="hero-content flex-col lg:flex-row-reverse">
+        <div class="hero-overlay bg-opacity-60 "></div>
+        <div class="hero-content flex-col lg:flex-row-reverse ">
           <div class="text-center lg:text-left">
             <h1 class="text-5xl font-bold">Open Electricity Market.</h1>
             <p class="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>

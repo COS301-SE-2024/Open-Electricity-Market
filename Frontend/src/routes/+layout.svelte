b09c23e89243e67e31913d37b58ef0c6788dee96
mihaildicoski <133096633+mihaildicoski@users.noreply.github.com>
2024-07-01 13:35:10 +0200

    Animations on page transition added

--- Frontend/src/routes/+layout.svelte
+++ Frontend/src/routes/+layout.svelte
@@ -14,16 +14,19 @@
 
   
 
-
+import { fade } from "svelte/transition";
+import { cubicIn, cubicOut } from "svelte/easing";
+import { fly } from "svelte/transition";
+export let data; 
 </script>
 
-<style>
- 
-
-</style>
 
 
-
-<main class="container mx-auto ">
-  <slot />
-</main>
+{#key data.pathname}
+    <div
+    in:fly={{ easing: cubicOut, y: 10, duration: 300, delay: 500 }}
+    out:fly={{ easing: cubicIn, y: -10, duration: 300 }}
+    >
+		<slot />
+	</div>
+{/key}

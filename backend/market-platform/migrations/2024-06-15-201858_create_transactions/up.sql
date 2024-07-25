0257c3a76fa063c60b378c871d38ddb8c6f0d5e4
ruanvdw <u21633020@tuks.co.za>
2024-07-25 09:43:13 +0200

    Added buy orders, modified transactions

--- backend/market-platform/migrations/2024-06-15-201858_create_transactions/up.sql
+++ backend/market-platform/migrations/2024-06-15-201858_create_transactions/up.sql
@@ -1,14 +1,24 @@
-CREATE TABLE transactions(
-    transaction_id bigserial PRIMARY KEY,
+CREATE TABLE buy_orders(
+    buy_order_id bigserial PRIMARY KEY,
     buyer_id uuid NOT NULL,
-    advertisement_id bigserial NOT NULL,
-    bought_units float8 NOT NULL,
+    sought_units NOT NULL DEFAULT 0,
+    price NOT NULL DEFAULT 0,
     CONSTRAINT fk_buyer_id
         FOREIGN KEY (buyer_id)
         REFERENCES users(user_id)
-        ON DELETE CASCADE,
-    CONSTRAINT fk_ad_id
-        FOREIGN KEY (advertisement_id)
-        REFERENCES advertisements(advertisement_id)
+        ON DELETE CASCADE
+)
+
+CREATE TABLE transactions(
+    transaction_id bigserial PRIMARY KEY,
+    sell_order_id bigserial NOT NULL,
+    buy_order_id bigserial NOT NULL,
+    CONSTRAINT fk_so_id
+        FOREIGN KEY (sell_order_id)
+        REFERENCES sell_order(sell_order_id)
+        ON DELETE CASCADE
+    CONSTRAINT fk_bo_id
+        FOREIGN KEY (buy_order_id)
+        REFERENCES buy_order(buy_order_id)
         ON DELETE CASCADE
 );

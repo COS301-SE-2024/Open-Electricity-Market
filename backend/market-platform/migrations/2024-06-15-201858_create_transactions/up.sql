e1ae43d4fcac63fc962c4479be539611af79ef9f
ruanvdw <u21633020@tuks.co.za>
2024-07-28 12:08:43 +0200

    Fixed CREATE TABLE statements for buy_orders and transactions

--- backend/market-platform/migrations/2024-06-15-201858_create_transactions/up.sql
+++ backend/market-platform/migrations/2024-06-15-201858_create_transactions/up.sql
@@ -1,13 +1,13 @@
 CREATE TABLE buy_orders(
     buy_order_id bigserial PRIMARY KEY,
     buyer_id uuid NOT NULL,
-    sought_units NOT NULL DEFAULT 0,
-    price NOT NULL DEFAULT 0,
+    sought_units float8 NOT NULL DEFAULT 0,
+    price float8 NOT NULL DEFAULT 0,
     CONSTRAINT fk_buyer_id
         FOREIGN KEY (buyer_id)
         REFERENCES users(user_id)
         ON DELETE CASCADE
-)
+);
 
 CREATE TABLE transactions(
     transaction_id bigserial PRIMARY KEY,
@@ -15,10 +15,10 @@ CREATE TABLE transactions(
     buy_order_id bigserial NOT NULL,
     CONSTRAINT fk_so_id
         FOREIGN KEY (sell_order_id)
-        REFERENCES sell_order(sell_order_id)
-        ON DELETE CASCADE
+        REFERENCES sell_orders(sell_order_id)
+        ON DELETE CASCADE,
     CONSTRAINT fk_bo_id
         FOREIGN KEY (buy_order_id)
-        REFERENCES buy_order(buy_order_id)
+        REFERENCES buy_orders(buy_order_id)
         ON DELETE CASCADE
 );

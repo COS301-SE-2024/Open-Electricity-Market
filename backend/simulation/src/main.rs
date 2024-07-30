50869b23a7e481ec9fbbb64868dd63a4cffaf05b
Simon Swart <u22490729@tuks.co.za>
2024-07-30 08:45:33 +0200

    Added stats endpoint for grid

--- backend/simulation/src/main.rs
+++ backend/simulation/src/main.rs
@@ -43,6 +43,13 @@ impl Fairing for CORS {
     }
 }
 
+
+
+
+
+
+
+
 #[get("/set_generator")]
 fn echo_channel(ws: ws::WebSocket, grid: &State<Arc<Mutex<Grid>>>) -> ws::Channel<'_> {
     use rocket::futures::{SinkExt, StreamExt};
@@ -65,6 +72,17 @@ fn echo_channel(ws: ws::WebSocket, grid: &State<Arc<Mutex<Grid>>>) -> ws::Channe
 fn index() -> String {
     "Yay".to_string()
 }
+
+
+#[post("/stats")]
+fn stats(grid : &State<Arc<Mutex<Grid>>>) -> content::RawJson<String> {
+  let g = grid.lock().unwrap();
+  let stats = g.get_grid_stats(); 
+  let stats = serde_json::to_string(&stats).unwrap();
+  content::RawJson(stats)
+}
+
+
 
 #[post("/info", format = "application/json")]
 fn info(grid: &State<Arc<Mutex<Grid>>>) -> content::RawJson<String> {
@@ -106,7 +124,7 @@ fn start(grid: &State<Arc<Mutex<Grid>>>) -> String {
 fn rocket() -> _ {
     rocket::build()
         .attach(CORS)
-        .mount("/", routes![index, start, info, echo_channel])
+        .mount("/", routes![index, start, info, echo_channel,stats])
         .manage(Arc::new(Mutex::new(Grid {
             circuits: vec![Circuit {
                 id: 0,

511c4f79ea1341159a37e2a70e16e723f9aa4915
Daniel Pierre de Villiers <u22509268@tuks.co.za>
2024-07-31 11:11:26 +0200

    Update dashboard page
    Node cards align, create node modal started
    Saving progress to merge dev - which has backend for this page

--- Frontend/src/routes/Main/Dashboard/+page.svelte
+++ Frontend/src/routes/Main/Dashboard/+page.svelte
@@ -59,7 +59,7 @@
   };
 
   function createModal(){
-    
+    document.getElementById("newNodeModal").showModal();
   }
 
 
@@ -80,48 +80,78 @@
         
       </div>
     </div>
+
+    <div class="card">
+      <div class="card-title">Personal Information</div>
+    </div>
   </div>
 
-  <div class="min-w-max min-h-fit mx-10">
+  <div class="min-w-max min-h-fit mx-10 flex-row">
 
-    <span class="text-3xl pt-8">Your Nodes</span> <br>
+    <div class="flex-col">
+      <span class="text-3xl justify-start">
+        Your Nodes
+      </span>
+      <span class="justify-end pl-96">
+        <button class="btn btn-primary text-lg " on:click={createModal}>Add a Node</button>
+      </span>
+    </div>
+
+    <dialog id="newNodeModal" class="modal">  
+      <div class="modal-box">
+        <h3 class="font-bold text-lg ">Add a Node</h3>
+        <form class="">
+          <div class="form-control mt-4">
+            <input class="input input-bordered" type="text" placeholder="Name">
+          </div>
+        </form>
+      </div>
+
+
+      <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+      </form>
+    </dialog>
+    
+
     {#each nodes as node}
-      <div class="card card-side min-w-full bg-base-300 my-2">
-        <figure class="w-2/5 p-10">
+      <div class="card card-side min-w-1/3 bg-base-300 my-2">
+        <figure class="w-1/5 p-10">
           <img
             src="../src/images/house.png"
             alt="House node" />
         </figure>
         <div class="card-body">
           <h2 class="card-title">Node {node}</h2>
-          <p>Current consumption: ...<br/>
-          Other stats: ...
+          <p class="">
+            Node type, etc
           </p>
           <div class="card-actions justify-end">
-            <button class="btn btn-primary">Transact</button>
+            <button class="btn btn-ghost">Details</button>
           </div>
         </div>
       </div>
-      
+
     {/each}
 
-    <button class="btn btn-primary text-lg" on:click={createModal}>Add a Node</button>
 
     
   </div>
   <div class="max-w-min mx-10">
     <div class="stats stats-vertical shadow"> 
       <div class="stat">
-        <div class="stat-title">Available Credit</div>
-        <div class="stat-value">R31,000</div>
+        <div class="stat-title">Total Comsumption</div>
+        <div class="stat-value">1,024W</div>
       </div>
     
       <div class="stat">
-        <div class="stat-title">Pending Transactions</div>
-        <div class="stat-value">5</div>
+        <div class="stat-title">Total Generation</div>
+        <div class="stat-value">5W</div>
         
       </div>
     </div>
+
+    <!-- node specific stats go here along with the transaction buttons -->
   </div>
 
 </main>

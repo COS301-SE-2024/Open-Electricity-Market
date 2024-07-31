b8abe15db9a6302a28ddf81d7f20f3c86f7ddb5e
ruanvdw <u21633020@tuks.co.za>
2024-07-31 13:20:01 +0200

    Update to register, sell orders get added

--- backend/market-platform/src/models.rs
+++ backend/market-platform/src/models.rs
@@ -16,9 +16,9 @@ pub struct User {
 
 #[derive(Insertable)]
 #[diesel(table_name = crate::schema::open_em::users)]
-pub struct NewUserModel<'a> {
-    pub email: &'a str,
-    pub pass_hash: &'a str,
+pub struct NewUserModel {
+    pub email: String,
+    pub pass_hash: String,
 }
 
 #[derive(Queryable, Selectable)]
@@ -31,41 +31,10 @@ pub struct Profile {
 
 #[derive(Insertable)]
 #[diesel(table_name = crate::schema::open_em::profiles)]
-pub struct NewProfileModel<'a> {
-    pub profile_user_id: &'a Uuid,
-    pub first_name: &'a str,
-    pub last_name: &'a str,
-}
-
-#[derive(Queryable, Selectable)]
-#[diesel(table_name = crate::schema::open_em::sell_orders)]
-#[diesel(check_for_backend(diesel::pg::Pg))]
-pub struct SellOrder {
-    pub sell_order_id: i64,
-    pub seller_id: Uuid,
-    pub created_at: DateTime<Utc>,
-    pub offered_units: f64,
-    pub claimed_units: f64,
-    pub price: f64,
-    pub producer_id: Uuid,
-}
-
-#[derive(Insertable)]
-#[diesel(table_name = crate::schema::open_em::sell_orders)]
-pub struct NewSellOrderModel<'a> {
-    pub seller_id: &'a Uuid,
-    pub producer_id: &'a Uuid,
-    pub offered_units: &'a f64,
-    pub price: &'a f64,
-}
-
-#[derive(Insertable)]
-#[diesel(table_name = crate::schema::open_em::buy_orders)]
-pub struct NewBuyOrderModel<'a> {
-    pub buyer_id: &'a Uuid,
-    pub consumer_id: &'a Uuid,
-    pub sought_units: &'a f64,
-    pub price: &'a f64,
+pub struct NewProfileModel {
+    pub profile_user_id: Uuid,
+    pub first_name: String,
+    pub last_name: String,
 }
 
 #[derive(Queryable, Selectable)]
@@ -99,6 +68,28 @@ pub struct BuyOrder {
     pub price: f64,
     pub created_at: DateTime<Utc>,
     pub consumer_id: Uuid,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = crate::schema::open_em::sell_orders)]
+pub struct NewSellOrder {
+    pub seller_id: Uuid,
+    pub offered_units: f64,
+    pub price: f64,
+    pub producer_id: Uuid,
+}
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::open_em::sell_orders)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct SellOrder {
+    pub sell_order_id: i64,
+    pub seller_id: Uuid,
+    pub created_at: DateTime<Utc>,
+    pub offered_units: f64,
+    pub claimed_units: f64,
+    pub price: f64,
+    pub producer_id: Uuid,
 }
 
 #[derive(Insertable)]

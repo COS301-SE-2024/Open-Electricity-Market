c9f7ddbd9b6b72b0377ff157373ec943690e5f21
Geordan <u22498878@tuks.co.za>
2024-08-01 13:15:06 +0200

    Setup test for redirect from signup to login page

--- Frontend/tests/example.spec.js
+++ Frontend/tests/example.spec.js
@@ -5,11 +5,10 @@ import {test, expect} from '@playwright/test';
 test('To login page', async ({ page }) => {
   await page.goto('http://localhost:5173');
 
-  // Click the signup button.
+  // Click the sign in button.
   await page.getByRole('link', { name: 'Sign in' }).click();
 
-  // Expects to be redirected to signup page.
-  //await expect(page).toHaveURL(/\/signup/);
+  // Expects to be redirected to login page.
   await expect(page).toHaveURL('http://localhost:5173/login');
 });
 test('To signup page', async ({ page }) => {
@@ -19,6 +18,14 @@ test('To signup page', async ({ page }) => {
   await page.getByRole('button', { name: 'Create an account' }).click();
 
   // Expects to be redirected to signup page.
-  //await expect(page).toHaveURL(/\/signup/);
   await expect(page).toHaveURL('http://localhost:5173/signup');
+});
+test('Back to login page', async ({ page }) => {
+  await page.goto('http://localhost:5173/signup');
+
+  // Click the "I already have an account" button.
+  await page.getByRole('button', { name: 'I already have an account' }).click();
+
+  // Expects to be redirected back to login page.
+  await expect(page).toHaveURL('http://localhost:5173/login',{timeout:3000});
 });

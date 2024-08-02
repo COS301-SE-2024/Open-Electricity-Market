834e4ba5e40e811299d071a4824ab1aaf44380bb
mihaildicoski <133096633+mihaildicoski@users.noreply.github.com>
2024-08-02 14:03:05 +0200

    animation on arrow - landing
    Co-authored-by: Daniel Pierre de Villiers <u22509268@tuks.co.za>

--- Frontend/src/routes/+page.svelte
+++ Frontend/src/routes/+page.svelte
@@ -29,8 +29,27 @@
                  <a class="btn bg-slate-800 text-xl" href="/login">Sign in</a>
                  <a class="btn bg-slate-800 text-xl" href="/public/GridSimulation">Simulation</a>
             </div>
-        </div>
+            <div class="belowcard flex flex-col w-max justify-center items-center">
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" class="pulse-1 -mb-[20px] svelte-1gnar4p" fill="#fff" viewBox="0 0 256 256">
+    <path d="M212.24,100.24l-80,80a6,6,0,0,1-8.48,0l-80-80a6,6,0,0,1,8.48-8.48L128,167.51l75.76-75.75a6,6,0,0,1,8.48,8.48Z"></path></svg>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" class="pulse-2 -mb-[20px] svelte-1gnar4p" fill="#fff" viewBox="0 0 256 256">
+    <path d="M212.24,100.24l-80,80a6,6,0,0,1-8.48,0l-80-80a6,6,0,0,1,8.48-8.48L128,167.51l75.76-75.75a6,6,0,0,1,8.48,8.48Z"></path></svg>
+
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" class="pulse-3 -mb-[20px] svelte-1gnar4p" fill="#fff" viewBox="0 0 256 256">
+    <path d="M212.24,100.24l-80,80a6,6,0,0,1-8.48,0l-80-80a6,6,0,0,1,8.48-8.48L128,167.51l75.76-75.75a6,6,0,0,1,8.48,8.48Z"></path></svg>
+
     </div>
+        </div>
+
+        
+        
+    </div>
+
+    
+    
+    
 
 </span>
 
@@ -72,7 +91,7 @@
 
 
 
-        <div class="card md:card-side glass shadow-xl mt-7">
+        <div class="card md:card-side glass shadow-xl mt-4">
             <figure class = "max-w-96 ">
                 <img src="../src/images/troy-bridges-maXnRLszYY0-unsplash.jpg" alt="img" />
             </figure>
@@ -119,6 +138,82 @@
   section {
     padding: 0vh 0 0vh;
   }
+
+  .pulse-1{
+     animation: pulse1 2s infinite ease-in-out;
+  }
+
+  .pulse-2{
+     animation: pulse2 2s infinite ease-in-out;
+  }
+
+  .pulse-3{
+     animation: pulse 2s infinite ease-in-out;
+  }
+
+  @keyframes pulse {
+     0% {
+        opacity: 1;
+    }
+    20% {
+        opacity:1;
+    }
+    40% {
+        opacity:1;
+    }
+    60% {
+        opacity: 0;
+    }
+    80% {
+        opacity: 0;
+    }
+    100% {
+        opacity: 1;
+    }
+  }
+
+  @keyframes pulse1 {
+    0% {
+        opacity: 1;
+    }
+    20% {
+        opacity:0;
+    }
+    40% {
+        opacity:0;
+    }
+    60% {
+        opacity: 1;
+    }
+    80% {
+        opacity: 1;
+    }
+    100% {
+        opacity: 1;
+    }
+  }
+
+  @keyframes pulse2 {
+    0% {
+        opacity: 1;
+    }
+    20% {
+        opacity:1;
+    }
+    40% {
+        opacity:0;
+    }
+    60% {
+        opacity: 0;
+    }
+    80% {
+        opacity: 1;
+    }
+    100% {
+        opacity: 1;
+    }
+  }
+
 </style>
 
 <!--<main>-->

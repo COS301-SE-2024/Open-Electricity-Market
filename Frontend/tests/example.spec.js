8cefce8e33edb819767ec4f37b7d00b5b50ac1c1
Geordan <u22498878@tuks.co.za>
2024-08-02 13:09:42 +0200

    Setting up test hooks to goto their respective pages before running

--- Frontend/tests/example.spec.js
+++ Frontend/tests/example.spec.js
@@ -2,14 +2,14 @@
 import {test, expect} from '@playwright/test';
 
 test.describe("Landing page",() => {
-  test('To login page', async ({ page }) => {
+  test.beforeEach(async ({page})=>{
     await page.goto('http://localhost:5173');
+  });
+  test('To login page', async ({ page }) => {
     // Click the sign in button.
-    //await page.getByRole('link', { name: 'Sign in' }).waitFor();
     await page.getByRole('link', { name: 'Sign in' }).click();
 
     // Expects to be redirected to login page.
-    //await expect(page).toHaveURL('http://localhost:5173/login');
     await page.waitForURL('**/login');
   });
 });
@@ -19,12 +19,10 @@ test.describe("login page",() => {
     await page.goto('http://localhost:5173/login');
     
     // Click the signup button.
-    //await page.getByRole('button', { name: 'Create an account' }).waitFor();
     await page.waitForLoadState('networkidle');
     await page.getByRole('button', { name: 'Create an account' }).click();
     
     // Expects to be redirected to signup page.
-    //await expect(page).toHaveURL('**/signup',{timeout:10500});
     await page.waitForURL('**/signup');
   });
 });
@@ -35,11 +33,10 @@ test.describe("signup page",() => {
 
     // Click the "I already have an account" button.
     await page.waitForLoadState('networkidle');
-    //await page.getByRole('button', { name: 'I already have an account' }).waitFor();
+
     await page.getByRole('button', { name: 'I already have an account' }).click();
 
     // Expects to be redirected back to login page.
-    //await expect(page).toHaveURL('**/login',{timeout:10500});
     await page.waitForURL('**/login');
   });
 });

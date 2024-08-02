3fbabf522625c545f95c0489b784f451c044447b
Daniel Pierre de Villiers <u22509268@tuks.co.za>
2024-08-02 10:39:17 +0200

    Email feedback and show password working kinda
    Still need to work on password creation validation, but I want that to be somewhat interactive.

--- Frontend/src/routes/signup/+page.svelte
+++ Frontend/src/routes/signup/+page.svelte
@@ -7,8 +7,10 @@
   let firstname = '';
   let surname = '';
   let password = '';
-  let passwordValidate = '';
+  let password2 = '';
   let errormessage = "";
+  let validEmail = false;
+  let validPassword = false;
 
 
 
@@ -19,14 +21,53 @@
   // might separate it out to make it reactive and indicate which requirements are not met to the user
   var passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
 
-  async function create(){
-      
+  async function validateEmail() {
+    const emailElement = document.getElementById("email");
     if (!email.match(emailRegex)) {
-      errormessage = "Please enter a valid email address."
+      validEmail = false;
+      errormessage = "Please enter a valid email address.";
+
+      emailElement.classList.add("input-error");
+
       return;
     }
-    
-    if(password == passwordValidate)
+    // maybe have an email specific error message 
+    errormessage = "";
+    emailElement.classList.remove("input-error");
+
+    validEmail = true;
+  }
+
+  function passwordCreationHelp() {
+
+  }
+
+  function showPassword() {
+    var input = document.getElementById("pw");
+    if (input.type === "password") {
+      input.type = "text";
+    } else {
+      input.type = "password";
+    }
+  }
+
+  function showPassword2() {
+    var input = document.getElementById("pw2");
+    if (input.type === "password") {
+      input.type = "text";
+    } else {
+      input.type = "password";
+    }
+  }
+
+  async function create(){
+
+    if (!validEmail || !validPassword) {
+      errormessage = "Invalid email or password";
+      return;
+    }
+
+    if(password == password2)
     {
       const res = await fetch("http://localhost:8001/register", {
         method: "POST", 
@@ -58,7 +99,7 @@
   }
   
   function back(){
-    email = firstname = surname = password = passwordValidate = '';
+    email = firstname = surname = password = password2 = '';
     goto("/login");
   }
 </script>
@@ -67,16 +108,16 @@
         <div class="hero-overlay bg-opacity-60 "></div>
         <div class="hero-content flex-col lg:flex-row-reverse rounded-2xl bg-base-100">
           <div class="text-center lg:text-left">
-            <h1 class="text-5xl font-bold">Open Electricity Market.</h1>
-            <p class="py-6">Discover a revolutionary platform where you can buy and sell electricity in real-time.</p>
+            <h1 class="text-5xl font-bold">Amplify</h1>
+            <p class="py-6">Discover a revolutionary open market platform where you can buy and sell electricity in real-time.</p>
           </div>
           <div class="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
             <form class="card-body">
               
-              <h2 class="text-base font-semibold"> Create an account </h2>
+              <h2 class="text-base font-semibold ml-1"> Create an account </h2>
 
               <div class="form-control">
-                <input type="email" placeholder="Email" class="input input-bordered" required bind:value={email}/>
+                <input id="email" type="email" placeholder="Email" class="input input-bordered" required bind:value={email} on:change={validateEmail}/>
               </div>
 
               <div class="form-control mt-4">
@@ -88,23 +129,48 @@
               </div>
               
               <div class="form-control mt-4">
-                <input type="password" placeholder="Password" class="input input-bordered" required bind:value={password}/>
+                <input id="pw" type="password" placeholder="Password" class="input input-bordered" required bind:value={password} on:input={passwordCreationHelp}/>
+                
+                <!-- button with icon (this doesnt work) -->
+                <!-- <button type="button" on:click={showPassword} class="absolute inset-y-0 end-0 flex items-center z-20 px-3 cursor-pointer text-gray-400 rounded-e-md focus:outline-none focus:text-blue-600 dark:text-neutral-600 dark:focus:text-blue-500">
+                  <svg class="shrink-0 size-3.5" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                    <path class="hs-password-active:hidden" d="M9.88 9.88a3 3 0 1 0 4.24 4.24"></path>
+                    <path class="hs-password-active:hidden" d="M10.73 5.08A10.43 10.43 0 0 1 12 5c7 0 10 7 10 7a13.16 13.16 0 0 1-1.67 2.68"></path>
+                    <path class="hs-password-active:hidden" d="M6.61 6.61A13.526 13.526 0 0 0 2 12s3 7 10 7a9.74 9.74 0 0 0 5.39-1.61"></path>
+                    <line class="hs-password-active:hidden" x1="2" x2="22" y1="2" y2="22"></line>
+                    <path class="hidden hs-password-active:block" d="M2 12s3-7 10-7 10 7 10 7-3 7-10 7-10-7-10-7Z"></path>
+                    <circle class="hidden hs-password-active:block" cx="12" cy="12" r="3"></circle>
+                  </svg>
+                </button> -->
               </div>
 
-              <div class="form-control mt-4">
-                <input type="password" placeholder="Re-enter password" class="input input-bordered" required bind:value={passwordValidate}/>
+              <!-- Checkbox (these work)-->
+              <div class="flex ml-2">
+                <input on:click={showPassword} id="hs-toggle-password-checkbox" type="checkbox" class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800">
+                <label for="hs-toggle-password-checkbox" class="text-sm text-gray-500 ms-3 dark:text-neutral-400">Show</label>
               </div>
 
-              {#if errormessage != ''}
-                <h2 class="text-base font-semibold text-black bg-error rounded"> { errormessage } </h2>
-              {/if}
+              <div class="form-control mt-2">
+                <input id="pw2" type="password" placeholder="Re-enter password" class="input input-bordered" required bind:value={password2}/>
+              </div>
+
+              <!-- Checkbox -->
+              <div class="flex ml-2">
+                <input on:click={showPassword2} id="hs-toggle-password-checkbox-2" type="checkbox" class="shrink-0 mt-0.5 border-gray-200 rounded text-blue-600 focus:ring-blue-500 dark:bg-neutral-800 dark:border-neutral-700 dark:checked:bg-blue-500 dark:checked:border-blue-500 dark:focus:ring-offset-gray-800">
+                <label for="hs-toggle-password-checkbox-2" class="text-sm text-gray-500 ms-3 dark:text-neutral-400">Show</label>
+              </div>
+
+                <!-- {#if errormessage != ''}
+                  <h2 class="text-base font-semibold text-black bg-error rounded"> { errormessage } </h2>
+                {/if} -->
 
               <div class="form-control mt-4">
                 <button class="btn btn-primary" on:click={create}>Create account</button>
               </div>
               
               <div class="form-control mt-3">
-                <button class="btn btn-primary" on:click={back}>I already have an account</button>
+                <!-- <button class="btn btn-primary" on:click={back}>I already have an account</button> -->
+                <a class="btn btn-outline btn-primary" href="/login">I already have an account</a>
               </div>
             </form>
           </div>

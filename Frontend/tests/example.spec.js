a2dce650e0988914449eea346c73926c4f98b462
Geordan <u22498878@tuks.co.za>
2024-08-05 14:05:04 +0200

    Added negative test that checks if one can create an account without giving any information
    Also found out cause for redirect issue for public simulation tests.

--- Frontend/tests/example.spec.js
+++ Frontend/tests/example.spec.js
@@ -97,5 +97,16 @@ test.describe("signup page",() => {
     // Expects to be redirected back to login page.
     await page.waitForURL('**/login');
   });
+  test('Sec:Signup with no Input', async ({ page }) => {
+    //Wait for page to finish loading
+    test.describe.configure({retries:1})
+    await page.waitForLoadState('networkidle');
+
+    // Click the "Create account" button.
+    await page.getByRole('button', { name: 'Create account' }).click();
+
+    //Tests if one can create an account without filling any of the forms.
+    await page.waitForURL('**/login');
+  });
 });
 

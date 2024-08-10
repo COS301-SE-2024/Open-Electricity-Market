5901285b7c7e27d2a4648ed84a4b78a4f83045f2
mihaildicoski <133096633+mihaildicoski@users.noreply.github.com>
2024-08-10 17:47:23 +0200

    Specified icon for marker to point to local copy of image.

--- Frontend/src/lib/Components/MapLanding.svelte
+++ Frontend/src/lib/Components/MapLanding.svelte
@@ -4,7 +4,10 @@
   import { onMount, onDestroy, createEventDispatcher } from 'svelte';
   import { browser } from '$app/environment';
   import Chart from './Chart2.svelte';
-  
+  import Chart2 from './Chart2.svelte';
+  // import leaflet from 'leaflet'; 
+  import iconmarkerpng from '../../images/marker-icon.png';
+
   let mapContainer;
   let map;
   let lm; 
@@ -27,28 +30,37 @@
 
       
       leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
+
+      const markerIcon = leaflet.icon({
+        iconUrl: iconmarkerpng,
+        iconSize: [25, 41], 
+        iconAnchor: [12, 41], 
+        popupAnchor: [1, -34], 
+        shadowSize: [41, 41], 
+        shadowAnchor: [12, 41]
+      });
       
-      lm = leaflet.marker([-26.1925013,28.0100383]).addTo(map);
-      leaflet.marker([-26.1825013, 28.0023383]).addTo(map);
-      leaflet.marker([-26.1725013, 28.0223383]).addTo(map);
-      leaflet.marker([-26.2025013, 28.0323383]).addTo(map);
-      leaflet.marker([-26.2125013, 28.0123383]).addTo(map);
-      leaflet.marker([-26.1925013, 28.0423383]).addTo(map);
-      leaflet.marker([-26.2225013, 28.0523383]).addTo(map);
-      leaflet.marker([-26.2325013, 28.0223383]).addTo(map);
-      leaflet.marker([-26.2025013, 28.0623383]).addTo(map);
-      leaflet.marker([-26.2425013, 28.0723383]).addTo(map);
-      leaflet.marker([-26.2525013, 28.0323383]).addTo(map);
-      leaflet.marker([-26.2625013, 28.0423383]).addTo(map);
-      leaflet.marker([-26.2725013, 28.0123383]).addTo(map);
-      leaflet.marker([-26.1925013, 28.0823383]).addTo(map);
-      leaflet.marker([-26.2825013, 28.0223383]).addTo(map);
-      leaflet.marker([-26.1925013, 28.0923383]).addTo(map);
-      leaflet.marker([-26.2925013, 28.0123383]).addTo(map);
-      leaflet.marker([-26.1825013, 28.1023383]).addTo(map);
-      leaflet.marker([-26.1725013, 28.1123383]).addTo(map);
-      leaflet.marker([-26.2025013, 28.1223383]).addTo(map);
-      leaflet.marker([-26.2125013, 28.1323383]).addTo(map);
+      lm = leaflet.marker([-26.1925013,28.0100383], {icon:markerIcon}).addTo(map);
+      leaflet.marker([-26.1825013, 28.0023383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.1725013, 28.0223383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.2025013, 28.0323383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.2125013, 28.0123383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.1925013, 28.0423383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.2225013, 28.0523383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.2325013, 28.0223383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.2025013, 28.0623383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.2425013, 28.0723383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.2525013, 28.0323383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.2625013, 28.0423383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.2725013, 28.0123383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.1925013, 28.0823383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.2825013, 28.0223383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.1925013, 28.0923383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.2925013, 28.0123383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.1825013, 28.1023383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.1725013, 28.1123383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.2025013, 28.1223383], {icon: markerIcon}).addTo(map);
+      leaflet.marker([-26.2125013, 28.1323383], {icon: markerIcon}).addTo(map);
 
     
     }

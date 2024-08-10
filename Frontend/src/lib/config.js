da19e5819f709017fce8cab8f06646550357ef99
Simon Swart <u22490729@tuks.co.za>
2024-08-10 13:46:50 +0200

    Linked to deploye api

--- Frontend/src/lib/config.js
+++ Frontend/src/lib/config.js
@@ -1,5 +1,5 @@
 // config.js
-export const API_URL_GRID = "";
-export const API_URL_MARKET = "";
+export const API_URL_GRID = "https://grid.amplify.org.za";
+export const API_URL_MARKET = "https://market.amplify.org.za";
 
 //endpoints need to be defined

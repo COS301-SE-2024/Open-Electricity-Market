e13cfe47244e90c44f6bd15817202631c2c92a78
Daniel Pierre de Villiers <133209869+Danieldv-s@users.noreply.github.com>
2024-08-19 10:08:11 +0200

    Update config.js

--- Frontend/src/lib/config.js
+++ Frontend/src/lib/config.js
@@ -2,5 +2,3 @@
 export const API_URL_GRID = "https://grid.amplify.org.za";
 export const API_URL_MARKET = "https://market.amplify.org.za";
 export const COOKIE_DOMAIN = "amplify.org.za";
-
-//endpoints need to be defined

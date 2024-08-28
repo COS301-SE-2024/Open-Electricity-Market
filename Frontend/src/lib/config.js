8d41389291e994c281a5b8944fed5d1cf1681c64
Simon Swart <u22490729@tuks.co.za>
2024-08-28 11:48:52 +0200

    Basics

--- Frontend/src/lib/config.js
+++ Frontend/src/lib/config.js
@@ -1,8 +1,8 @@
 // config.js
-export const API_URL_GRID = "https://grid.amplify.org.za";
-export const API_URL_MARKET = "https://market.amplify.org.za";
-export const API_URL_AGENT = "https://agent.amplifty.org.za";
-export const COOKIE_DOMAIN = "amplify.org.za";
+export const API_URL_GRID = "https://grid.site.localhost";
+export const API_URL_MARKET = "https://market.site.localhost";
+export const API_URL_AGENT = "https://agent.site.localhost";
+export const COOKIE_DOMAIN = "site.localhost";
 
 // export const API_URL_GRID = "https://grid.site.localhost";
 // export const API_URL_MARKET = "https://market.site.localhost";

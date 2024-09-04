d6eb80cd0e38ca97512f0027cbd084819f99102a
Simon Swart <u22490729@tuks.co.za>
2024-09-04 09:14:20 +0200

    Made query work

--- backend/market-platform/src/schema.rs
+++ backend/market-platform/src/schema.rs
@@ -9,23 +9,10 @@ pub mod open_em {
     }
 
     diesel::table! {
-        open_em.air_conditioner (time) {
+        open_em.appliance_data (time, appliance) {
             time -> Timestamptz,
             data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.air_purifier (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.boiler (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
+            appliance -> Text,
         }
     }
 
@@ -43,86 +30,9 @@ pub mod open_em {
     }
 
     diesel::table! {
-        open_em.coffee (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.computer (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.dehumidifier (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.dishwasher (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.dryer (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.fan (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.freezer (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.fridge (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
         open_em.grid_history (created_at) {
             created_at -> Timestamptz,
             grid_state -> Nullable<Jsonb>,
-        }
-    }
-
-    diesel::table! {
-        open_em.internet_router (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.laptop (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.micro_wave_oven (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
         }
     }
 
@@ -138,45 +48,10 @@ pub mod open_em {
     }
 
     diesel::table! {
-        open_em.phone_charger (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.printer (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.printer_3d (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
         open_em.profiles (profile_user_id) {
             profile_user_id -> Uuid,
             first_name -> Text,
             last_name -> Text,
-        }
-    }
-
-    diesel::table! {
-        open_em.radiator (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.screen (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
         }
     }
 
@@ -190,20 +65,6 @@ pub mod open_em {
             max_price -> Float8,
             min_price -> Float8,
             producer_id -> Uuid,
-        }
-    }
-
-    diesel::table! {
-        open_em.solar_panel (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.sound_system (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
         }
     }
 
@@ -222,13 +83,6 @@ pub mod open_em {
     }
 
     diesel::table! {
-        open_em.tv (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
         open_em.users (user_id) {
             user_id -> Uuid,
             email -> Text,
@@ -236,20 +90,6 @@ pub mod open_em {
             credit -> Float8,
             active -> Bool,
             created_at -> Timestamptz,
-        }
-    }
-
-    diesel::table! {
-        open_em.vacuum (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
-        }
-    }
-
-    diesel::table! {
-        open_em.washing_machine (time) {
-            time -> Timestamptz,
-            data -> Nullable<Float8>,
         }
     }
 
@@ -264,36 +104,13 @@ pub mod open_em {
 
     diesel::allow_tables_to_appear_in_same_query!(
         agent_history,
-        air_conditioner,
-        air_purifier,
-        boiler,
+        appliance_data,
         buy_orders,
-        coffee,
-        computer,
-        dehumidifier,
-        dishwasher,
-        dryer,
-        fan,
-        freezer,
-        fridge,
         grid_history,
-        internet_router,
-        laptop,
-        micro_wave_oven,
         nodes,
-        phone_charger,
-        printer,
-        printer_3d,
         profiles,
-        radiator,
-        screen,
         sell_orders,
-        solar_panel,
-        sound_system,
         transactions,
-        tv,
         users,
-        vacuum,
-        washing_machine,
     );
 }

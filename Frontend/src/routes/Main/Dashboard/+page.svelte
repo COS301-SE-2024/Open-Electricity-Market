97853c08fe9e7bfbcbb3edc4867b2c15df245a21
Geordan <u22498878@tuks.co.za>
2024-09-11 19:38:03 +0200

    Dynamic sized nodes card

--- Frontend/src/routes/Main/Dashboard/+page.svelte
+++ Frontend/src/routes/Main/Dashboard/+page.svelte
@@ -525,16 +525,16 @@
 
 </script>
 
-<main class="container sm:mx-auto w-full h-full sm:flex justify-center">
+<main class="container sm:mx-auto w-full h-screen sm:flex justify-center">
   <!--first-->
-  <div class="sm:w-1/3">
+  <div class="sm:w-1/3 h-screen flex flex-col">
     
     <!--Personal Info-->
     <span class="text-3xl text-white font-thin justify-start pl-2">
       Personal Information
     </span>
     
-    <div class="stats stats-vertical w-full"> 
+    <div class="h-1/2 stats stats-vertical w-full"> 
       <div class="stat">
         <div class="stat-title">Available Credit</div>
         {#if totalamount == null}
@@ -639,15 +639,15 @@
   </div>
 
   <!--second-->
-  <div class="sm:w-1/3 mx-4 flex-row">
+  <div class="sm:w-1/3 h-screen mx-4 flex flex-col">
 
     <!--Nodes-->
     <span class="basis text-3xl text-white font-thin justify-start pl-2">
       Your Nodes
     </span>
-    <div class="flex flex-col">
+    <div class="h-1/2 flex flex-col">
       
-      <div class = "card bg-base-100 min-h-80 max-h-80 overflow-y-auto ">
+      <div class = "card bg-base-100 overflow-y-auto">
         {#each nodes as node}
         {#if node.name == nodeNameDetail}
           <div class="card card-side border-4 border-primary min-w-1/3 bg-base-100 mb-2">
@@ -680,6 +680,7 @@
         {/if}
         {/each}
       </div>
+
       <!--Add New node-->
       <div class="card card-side min-w-1/3 bg-base-100 mt-2">
         <div class="card-body">

840b317445c446c4c6239af9b45c5a98de6fba20
Geordan <u22498878@tuks.co.za>
2024-09-12 12:14:48 +0200

    Final adjustments

--- Frontend/src/routes/Main/Dashboard/+page.svelte
+++ Frontend/src/routes/Main/Dashboard/+page.svelte
@@ -648,13 +648,13 @@
         {#each nodes as node}
         {#if node.name == nodeNameDetail}
           <div class="card card-side border-4 border-primary min-w-1/3 bg-base-100 mb-2">
-            <figure class="w-1/5 p-10">
+            <figure class="w-1/4 p-3 pr-0">
               <img
                 src="../src/images/house.png"
                 alt="House node" />
             </figure>
-            <div class="card-body">
-              <h2 class="card-title font-light text-3xl">{node.name}</h2>
+            <div class="card-body pb-4 px-4">
+              <h2 class="card-title font-light text-2xl">{node.name}</h2>
               <div class="card-actions justify-end">
                 <button class="btn btn-primary" on:click={() => {fetchNodeDetails(node.node_id)}}>Details</button>
               </div>
@@ -662,13 +662,13 @@
           </div>  
         {:else}
           <div class="card card-side border-4 border-base-100 min-w-1/3 bg-base-100 mb-2">
-            <figure class="w-1/5 p-10">
+            <figure class="w-1/4 p-3 pr-0">
               <img
                 src="../src/images/house.png"
                 alt="House node" />
             </figure>
-            <div class="card-body">
-              <h2 class="card-title font-light text-3xl">{node.name}</h2>
+            <div class="card-body pb-4 px-4">
+              <h2 class="card-title font-light text-2xl">{node.name}</h2>
               <div class="card-actions justify-end">
                 <button class="btn btn-primary" on:click={() => {fetchNodeDetails(node.node_id)}}>Details</button>
               </div>

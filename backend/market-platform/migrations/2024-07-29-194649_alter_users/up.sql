4f9ef1678e69b6afd255c828b57a4afb218b51c5
ruanvdw <u21633020@tuks.co.za>
2024-09-13 15:15:46 +0200

    Update to triggers on user deactivation and reactivation

--- backend/market-platform/migrations/2024-07-29-194649_alter_users/up.sql
+++ backend/market-platform/migrations/2024-07-29-194649_alter_users/up.sql
@@ -1,2 +1,40 @@
 ALTER TABLE users
 ADD COLUMN created_at timestamptz NOT NULL DEFAULT now();
+
+CREATE OR REPLACE FUNCTION set_nodes_inactive()
+    RETURNS TRIGGER
+AS $$
+BEGIN
+    IF (NEW.active = FALSE) THEN
+        UPDATE nodes
+        SET node_active = FALSE
+        WHERE node_owner = NEW.user_id;
+    END IF;
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER deactivate_node
+    AFTER UPDATE
+    ON users
+    FOR EACH ROW
+EXECUTE FUNCTION set_nodes_inactive();
+
+CREATE OR REPLACE FUNCTION set_nodes_active()
+    RETURNS TRIGGER
+AS $$
+    BEGIN
+        IF (OLD.active = FALSE) THEN
+            UPDATE nodes
+            SET node_active = TRUE
+            WHERE node_owner = OLD.user_id;
+        END IF;
+        RETURN NEW;
+    END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER reactivate_node
+    AFTER UPDATE
+    ON users
+    FOR EACH ROW
+EXECUTE FUNCTION set_nodes_active();

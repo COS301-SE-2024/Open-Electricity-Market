3bf26da0ab5f8c6611105423a7391c37d44ea09f
Simon Swart <u22490729@tuks.co.za>
2024-09-13 15:16:04 +0200

    Bug fixing is fun
    But not yet done

--- backend/agent/src/main.rs
+++ backend/agent/src/main.rs
@@ -571,16 +571,6 @@ fn add_agent(
     let id = agents.len() - 1;
     agents[id].market_token = data.token;
     agents[id].intialise();
-    tokio::spawn(async move {
-        let mut accumilated_time = 0.0;
-        loop {
-            {
-                let mut agent = lock.lock().unwrap();
-                accumilated_time = agent[id].async_run(accumilated_time);
-            }
-            thread::sleep(time::Duration::from_secs(AGENT_SPEED));
-        }
-    });
 
     let message = "Succesfully added agent".to_string();
     content::RawJson(json!({"status": "ok", "message": message, "data": {}}).to_string())
@@ -642,18 +632,31 @@ fn rocket() -> _ {
     let agents = Arc::new(Mutex::new(Vec::<Agent>::new()));
     let agents_clone = agents.clone();
 
-    thread::spawn(move || loop {
-        {
-            use crate::agent_history::dsl::agent_history;
-            let agents = agents_clone.lock().unwrap();
-            let santas_address: serde_json::Value =
-                serde_json::from_str(&serde_json::to_string(agents.deref()).unwrap())
-                    .expect("REASON");
-            let _ = insert_into(agent_history)
-                .values(agent_state.eq(santas_address))
-                .execute(&mut establish_connection());
+    thread::spawn(move || {
+        let mut count = 0;
+        loop {
+            {
+                let mut agents = agents_clone.lock().unwrap();
+                for agent in agents.iter_mut() {
+                    agent.async_run(0.0);
+                }
+
+                if count > 5 {
+                    use crate::agent_history::dsl::agent_history;
+
+                    count = 0;
+                    let santas_address: serde_json::Value =
+                        serde_json::from_str(&serde_json::to_string(agents.deref()).unwrap())
+                            .expect("REASON");
+                    let _ = insert_into(agent_history)
+                        .values(agent_state.eq(santas_address))
+                        .execute(&mut establish_connection());
+                } else {
+                    count += 1;
+                }
+            }
+            thread::sleep(time::Duration::from_secs(AGENT_SPEED))
         }
-        thread::sleep(time::Duration::from_secs(50))
     });
 
     rocket::build()

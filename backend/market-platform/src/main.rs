c26ce7616423452ce595cbd38133d52587dbed5c
ruanvdw <u21633020@tuks.co.za>
2024-09-29 13:12:32 +0200

    Linter fixes

--- backend/market-platform/src/main.rs
+++ backend/market-platform/src/main.rs
@@ -20,8 +20,8 @@ use rocket::{Request, Response};
 use std::env;
 
 const TRANSACTION_LIFETIME: i64 = 2; // Lifetime in hours
-                                      // const TARGET_VOLTAGE: f64 = 240.0;
-                                      // Endpoint for current_voltage
+                                     // const TARGET_VOLTAGE: f64 = 240.0;
+                                     // Endpoint for current_voltage
 
 const UNIT_PRICE_RATE: f64 = 0.005;
 const IMPEDANCE_RATE: f64 = 0.05;

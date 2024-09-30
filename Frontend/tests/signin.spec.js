8cc4eb971523673973d7038bd4c89a6ff5ed76c3
Geordan <u22498878@tuks.co.za>
2024-09-30 12:14:42 +0200

    Testing funds

--- Frontend/tests/signin.spec.js
+++ Frontend/tests/signin.spec.js
@@ -21,5 +21,15 @@ test.describe("signed in to dashboard", () => {
         await page.getByRole('heading', { name: 'Dashboard' }).click();
         await page.getByText('This is the central hub for').click();
     });
+    test("funds", async ({page}) => {
+        await page.getByText('R 0.00');
+        await page.getByRole('button', { name: 'Add funds' }).click();
+        await page.locator('#add_modal').getByPlaceholder('Amount').click();
+        await page.locator('#add_modal').getByPlaceholder('Amount').fill('1000');
+        await page.locator('#add_modal').getByRole('button', { name: 'Continue' }).click();
+        await page.getByRole('heading', { name: 'You have successfully added funds!' }).click();
+        await page.getByText('You have successfully added R 1000.00').click();
+        await page.getByText('R 1000.00').click();
+    });
 
 });
